c04d7304074e1ca981c9c2ccaa19c757c6ec7633
Gao Wang <gaow@uchicago.edu>
2019-08-29 16:14:12 -0500

    Various fixes

--- R/mirage.R
+++ R/mirage.R
@@ -1,51 +1,48 @@
 #' Mixture model based rare variant analysis
 #'
-#' This function implements the gene based version of rare variant test
-#' with MIRAGE model
-#' @param data variant count summary statistics, a 4 column data frame for 1) loci ID, 2) count in group 1, 
-#' 3) count in group 2 and 4) functional category for a variant.
+#' This function implements rare variant test with MIRAGE model
+#'
+#' @param data variant count data, a 4 column data frame for 1) locus ID, 2) count in group 1, 
+#' 3) count in group 2 and 4) category for a variant. The 4th column is optional.
 #' @param n1 sample size for group 1; either an integer number if all variants have the same sample size, or a vector
 #' of length of number of variants to specify sample size for each variant.
 #' @param n2 sample size for group 2; either an integer number if all variants have the same sample size, or a vector
 #' of length of number of variants to specify sample size for each variant.
-#' @param gamma a vector of category specific effect size prior
-#' @param sigma prior on proportion of risk genes
-#' @param delta ... FIXME
-#' @param eta.init ... FIXME (do you have default eta.init? zero?)
+#' @param gamma a list of category specific shape parameter for effect size prior, or a numeric value if all category share the same effect size.
+#' @param sigma a list of category specific scale parameter for effect size prior, or a numeric value if all category share the same effect size.
+#' @param delta.init initial value for prior on proportion of risk genes.
+#' @param eta.init initial value for prior on proportion of risk variants in a risk gene.
 #' @return \item{BF}{Bayes factor of genes}
-#' \item{delta.est}{...}
-#' \item{delta.pvalue}{...}
-#' \item{eta.est}{...}
-#' \item{pvalue}{...}
+#' \item{delta.est}{Estimate for proportion of risk genes}
+#' \item{delta.pvalue}{Significant test for delta = 0}
+#' \item{eta.est}{Estimate for proportion of risk variants in a risk gene}
+#' \item{eta.pvalue}{Significant test for eta = 0}
+#' \item{BF.gene}{Bayes factor for each gene}
+#' \item{BF.all}{Bayes factor for each variant}
 #' @examples
 #' mirage(...)
 #' @importFrom progress progress_bar
 #' @export
-mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter = 10000, 
+mirage = function(data, n1, n2, gamma=3, sigma=1, eta.init=0, delta.init=0, estimate.delta = TRUE, max.iter = 10000, 
     tol = 1e-05, verbose = TRUE) {
     # Input check & initialize
-    if (!is.null(eta.init)) {
-        eta.k[1, ] = eta.init
-     } else {
-         stop("FIXME: need default input")
-     }
+    if (ncol(data) == 3) data = c(data, 0)
+    if (ncol(data) != 4) stop("Input data should have 3 or 4 columns!")
     names(data) = c("name", "C1", "C2", "category")
     unique.gene = unique(data$name)
     num.gene = length(unique.gene)
-    num.group = length(unique(data$category))
+    groups = unique(data$category)
+    num.group = length(groups)
+    data[,4] = match(data[,4], groups)
     eta.k = matrix(nrow = max.iter, ncol = num.group)
+    colnames(eta.k) = groups
+    eta.k[1, ] = rep(eta.init, num.group)
+    delta.est = delta.init
     BF.gene = matrix(1, nrow = max.iter, ncol = num.gene)
-    LoF.BF.gene = matrix(1, nrow = max.iter, ncol = num.gene)
-    nonLoF.BF.gene = matrix(1, nrow = max.iter, ncol = num.gene)
+    colnames(BF.gene) = unique.gene
     BF.genevar = list()
-    delta.est = numeric()
-    if (!is.null(delta)) { 
-        delta.est[1] = delta
-    } else {
-        stop("FIXME: need default input")
-    }
     # progress bar
-    if (verbose) {
+    if (verbose && num.gene > 1) {
         pb = progress_bar$new(format = "[:spin] Initial analysis of unit :unit out of :total :elapsed", 
             clear = FALSE, total = num.gene, show_after = 0.5) 
     } else { 
@@ -53,39 +50,33 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
     }
     # calculate the Bayes factor for variant j in gene i
     for (i in 1:num.gene) {
-        var.index.list = which(data$name == unique.gene[i])
-        var.BF = numeric()
-        if (length(var.index.list) > 0) {
+        var.names = rownames(data)[which(data$name == unique.gene[i])]
+        BF.var = numeric()
+        if (length(var.names) > 0) {
             # calculate Bayes factor for variant (i,j)
-            for (j in 1:length(var.index.list)) {
-                category = data$category[var.index.list[j]]
-                # FIXME: explain what 5 does here
-                if (category <= 5) {
-                  # FIXME: have to handle N1 N2 being vectors
-                  var.BF[j] = BF.var.inte(data$C1[var.index.list[j]], data$C2[var.index.list[j]], 
-                    bar.gamma = 6, sig = sigma, N1, N2)
-                } else {
-                  var.BF[j] = BF.var.inte(data$C1[var.index.list[j]], data$C2[var.index.list[j]], 
-                    bar.gamma = gamma.mean, sig = sigma, N1, N2)
-                }
-                mixed_bf = 1 - eta.k[1, category] + eta.k[1, category] * var.BF[j]
+            for (j in 1:length(var.names)) {
+                category = data[var.names[j],]$category
+                # FIXME: have to handle sample size being vectors
+                BF.var[j] = BF.var.inte(data[var.names[j],]$C1, data[var.names[j],]$C2,
+                                    ifelse(class(gamma) == "list", gamma[category], gamma), 
+                                    ifelse(class(sigma) == "list", sigma[category], sigma), 
+                                    n1, n2)
+                mixed_bf = 1 - eta.k[1, category] + eta.k[1, category] * BF.var[j]
                 BF.gene[1, i] = BF.gene[1, i] * mixed_bf
-                ################## split BF of LoF and non LoF FIXME: explain what 2 does here
-                if (category <= 2) }{ 
-                  LoF.BF.gene[1, i] = LoF.BF.gene[1, i] * mixed_bf
-                 } else {
-                     nonLoF.BF.gene[1, i] = nonLoF.BF.gene[1, i] * mixed_bf
-                 }
+                # FIXME: need to add a utility function that extract BF from given annotation category
             }
         }
-        BF.genevar[[i]] = cbind(var.index.list, var.BF)
-        colnames(BF.genevar[[i]]) = c("idx", "BF")
+        BF.genevar[[unique.gene[i]]] = cbind(var.names, BF.var)
+        colnames(BF.genevar[[unique.gene[i]]]) = c("variant", "BF")
         pb$tick(tokens = list(total = num.gene, unit = i))
     }
     # EM algorithm
-    if (verbose) 
+    if (verbose) {
         pb = progress_bar$new(format = "[:spin] Iteration :iteration (diff = :delta) :elapsed", 
             clear = TRUE, total = max.iter, show_after = 0.5)
+    } else { 
+        pb = null_progress_bar$new()
+    }
     for (iter in 2:max.iter) {
         prev_iter = iter - 1
         # E step expectation for variant (i,j), every gene may have varying number of
@@ -97,38 +88,36 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
         total.Ui = matrix(nrow = num.gene, ncol = num.group)  # used to estimate eta
         for (i in 1:num.gene) {
             UiZij = numeric()
-            if (nrow(BF.genevar[[i]]) > 0) {
-                for (j in 1:nrow(BF.genevar[[i]])) {
-                  category = data$category[BF.genevar$idx[j]]
-                  numer = BF.genevar[[i]]$BF[j] * eta.k[prev_iter, category] * delta.est[prev_iter]
+            BF.var = BF.genevar[[unique.gene[i]]]
+            if (nrow(BF.var) > 0) {
+                for (j in 1:nrow(BF.var)) {
+                  category = data[BF.var[,1][j],]$category
+                  print(class(BF.var[,2][j]))
+                  print(BF.var[,2][j])
+                  print(class(eta.k[prev_iter, category]))
+                  print(eta.k[prev_iter, category])
+                  numer = BF.var[,2][j] * eta.k[prev_iter, category] * delta.est[prev_iter]
                   denom = (delta.est[prev_iter] + (1 - delta.est[prev_iter])/BF.gene[prev_iter, 
-                    i]) * (eta.k[prev_iter, category] * BF.genevar[[i]]$BF[j] + (1 - 
+                    i]) * (eta.k[prev_iter, category] * BF.var[,2][j] + (1 - 
                     eta.k[prev_iter, category]))
                   UiZij[j] = numer/denom
                   mixed_bf = 1 - eta.k[prev_iter, category] + eta.k[prev_iter, 
-                    category] * BF.genevar[[i]]$BF[j]
+                    category] * BF.var[,2][j]
                   BF.gene[iter, i] = BF.gene[iter, i] * mixed_bf
-                  ########################### split into LoF and non-LoF two parts
-                  if (category <= 2) {
-                    LoF.BF.gene[iter, i] = LoF.BF.gene[iter, i] * mixed_bf 
-                  } else {
-                      nonLoF.BF.gene[iter, i] = nonLoF.BF.gene[iter, i] * mixed_bf
-                  }
                 }
             }
             EUiZij[[i]] = UiZij
-            EUi[i] = delta.est[prev_iter] * bb/(delta.est[prev_iter] * bb + 1 - delta.est[prev_iter])
-            ###################### Note here each gene may have multiple annotation groups
+            EUi[i] = delta.est[prev_iter] * BF.gene[iter, i]/(delta.est[prev_iter] * BF.gene[iter, i] + 1 - delta.est[prev_iter])
+            ###################### Each gene may contain multiple annotation groups
             for (g in 1:num.group) {
-                total.Zij[i, g] = sum(UiZij[which(data$category[BF.genevar$idx] == 
+                total.Zij[i, g] = sum(UiZij[which(data[BF.var[,1],]$category == 
                   g)], na.rm = TRUE)
-                total.Ui[i, g] = sum(sum(UiZij[which(data$category[BF.genevar$idx] == 
+                total.Ui[i, g] = sum(sum(UiZij[which(data[BF.var[,1],]$category == 
                   g)] > 0, na.rm = TRUE) * EUi[i])
             }
         }
         ############## EM algorithm: M step
-        delta.est[iter] = sum(EUi)/num.gene
-        # delta.est[iter]=delta
+        delta.est[iter] = ifelse(estimate.delta, sum(EUi)/num.gene, delta)
         for (g in 1:num.group) {
             if (sum(total.Ui[, g]) != 0) 
                 eta.k[iter, g] = sum(total.Zij[, g])/sum(total.Ui[, g])
@@ -136,8 +125,7 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
                 eta.k[iter, g] = 0
         }
         ################ 
-        if (num.group > 1) 
-            diff = sum(abs(eta.k[iter, ] - eta.k[prev_iter, ]))
+        diff = sum(abs(eta.k[iter, ] - eta.k[prev_iter, ]))
         if (diff < tol || iter >= max.iter) {
             pb$tick(max.iter)
             max.iter = iter
@@ -145,12 +133,7 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
         }
         pb$tick(tokens = list(delta = sprintf(diff, fmt = "%#.1e"), iteration = i))
     }  # end of iter
-    # FIXME: need to unify this.
-    if (num.group > 1) {
-        eta.k = eta.k[1:max.iter, ]
-    } else {
-        eta.k = eta.k[1:max.iter]
-    }
+    eta.k = eta.k[1:max.iter, , drop=FALSE]
     # calculate the LRT statistics and p-value for genes
     if (verbose) 
         cat("Computing gene level LRT statistics and p-values ...\n")
@@ -159,17 +142,18 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
     teststat = numeric()
     pvalue = numeric()
     for (i in 1:num.gene) {
-        if (nrow(BF.genevar[[i]]) > 0) {
-            for (j in 1:nrow(BF.genevar[[i]])) {
-                category = data$category[BF.genevar$idx[j]]
-                lkhd[i] = lkhd[i] * ((1 - eta.k[prev_iter, category]) + eta.k[prev_iter, 
-                  category] * BF.genevar$BF[j])
+        BF.var = BF.genevar[[unique.gene[i]]]
+        if (nrow(BF.var) > 0) {
+            for (j in 1:nrow(BF.var)) {
+                category = data[BF.var[,1][j],]$category
+                lkhd[i] = lkhd[i] * ((1 - eta.k[max.iter, category]) + eta.k[max.iter, 
+                  category] * BF.var[,2][j])
             }
         }
-        teststat[i] = 2 * log((1 - delta.est[prev_iter]) + delta.est[prev_iter] * 
+        teststat[i] = 2 * log((1 - delta.est[max.iter]) + delta.est[max.iter] * 
             lkhd[i])
         # this is the test statistics of one gene
-        total.lkhd = total.lkhd + log((1 - delta.est[prev_iter]) + delta.est[prev_iter] * 
+        total.lkhd = total.lkhd + log((1 - delta.est[max.iter]) + delta.est[max.iter] * 
             lkhd[i])
         pvalue[i] = pchisq(teststat[i], 2, lower.tail = F)
     }
@@ -177,7 +161,7 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
     pvalue[num.gene + 1] = pchisq(teststat[num.gene + 1], 2, lower.tail = F)
     # calculate the LRT statistics and p-value for categories
     if (verbose) 
-        cat("Computing LRT statistics and p-values by functional groups ...\n")
+        cat("Computing LRT statistics and p-values by categories ...\n")
     cate.lkhd = rep(1, num.group)
     sum.lkhd = 0
     cate.stat = numeric()
@@ -186,16 +170,16 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
         total.lkhd = 0
         lkhd.gene = rep(1, num.gene)
         for (i in 1:num.gene) {
-            if (nrow(BF.genevar[[i]]) > 0) 
-                for (j in 1:nrow(BF.genevar[[i]])) {
-                  if (data$category[BF.genevar$idx[j]] == g) {
-                    mixed_bf = 1 - eta.k[prev_iter, g] + eta.k[prev_iter, g] * 
-                      data$var.BF[j]
+            BF.var = BF.genevar[[unique.gene[i]]]
+            if (nrow(BF.var) > 0) 
+                for (j in 1:nrow(BF.var)) {
+                  if (data[BF.var[,1][j],]$category == g) {
+                    mixed_bf = 1 - eta.k[max.iter, g] + eta.k[max.iter, g] * BF.var[,1][j]
                     lkhd.gene[i] = lkhd.gene[i] * mixed_bf
                     cate.lkhd[g] = cate.lkhd[g] * mixed_bf
                   }
                 }
-            total.lkhd = total.lkhd + log((1 - delta.est[prev_iter]) + delta.est[prev_iter] * 
+            total.lkhd = total.lkhd + log((1 - delta.est[max.iter]) + delta.est[max.iter] * 
                 lkhd.gene[i])
         }  # end of i
         cate.stat[g] = 2 * total.lkhd
@@ -203,12 +187,10 @@ mirage = function(data, N1, N0, gamma, sigma, eta.init, delta = NULL, max.iter =
     }
     sum.lkhd = sum(cate.stat)
     ############################################## 
-    return(result = list(delta.est = delta.est[prev_iter], delta.pvalue = pvalue[length(pvalue)], 
-        eta.est = eta.k[prev_iter, ], eta.stat = cate.stat, eta.pvalue = cate.pvalue, 
-        BF.gene = data.frame(Gene = unique.gene, BF = BF.gene[prev_iter, ], LoF.BF = LoF.BF.gene[prev_iter, 
-            ], nonLoF.BF = nonLoF.BF.gene[prev_iter, ]), BF.all = BF.genevar, Eui = EUi))
+    return(result = list(delta.est = delta.est[max.iter], delta.pvalue = pvalue[length(pvalue)], 
+        eta.est = eta.k[max.iter, ], eta.pvalue = cate.pvalue, 
+        BF.gene = data.frame(Gene = unique.gene, BF = BF.gene[max.iter, ]), BF.all = BF.genevar, Eui = EUi))
 }
-
 
 intergrand = function(aa, var.case, var.contr, bar.gamma, sig, N1, N0) {
     ff = dbinom(var.case, sum(var.case, var.contr), aa * N1/(aa * N1 + N0)) * dgamma(aa, 
